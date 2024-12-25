6fee9787941a60e0f4a16b69eec3319d105fa5f8
ReijiTerunuma <reiji.terunuma@newinov.com>
2024-12-25 21:00:40 +0900

    perf: remove ownership for adc & tim

--- sensors2/src/infrared.rs
+++ sensors2/src/infrared.rs
@@ -4,62 +4,61 @@ use nb::block;
 // use uom::si::{f32::Length, length::meter, ratioratio::};
 use spin::Mutex;
 
-pub struct Infrared<T, ADC, AdcPin, TimPin>
+pub struct Infrared<ADC, AdcPin>
 where
-    T: OneShot<ADC, u16, AdcPin>,
     AdcPin: Channel<ADC>,
-    TimPin: PwmPin,
 {
-    adc: Mutex<T>,
     adc_pin: AdcPin,
     value: u16,
     _adc_marker: PhantomData<ADC>,
-    tim_pin: Mutex<TimPin>,
     ratio: f32,
 }
 
-impl<T, ADC, AdcPin, TimPin> Infrared<T, ADC, AdcPin, TimPin>
+impl<ADC, AdcPin> Infrared<ADC, AdcPin>
 where
-    T: OneShot<ADC, u16, AdcPin>,
     AdcPin: Channel<ADC>,
-    <T as OneShot<ADC, u16, AdcPin>>::Error: core::fmt::Debug,
-    TimPin: PwmPin<Duty = u16>,
 {
-    pub fn new(adc: Mutex<T>, adc_pin: AdcPin, tim_pin: Mutex<TimPin>, duty_ratio: f32) -> Self {
+    pub fn new(adc_pin: AdcPin, duty_ratio: f32) -> Self {
         let mut infrared = Self {
-            adc,
             adc_pin,
             value: 0,
             _adc_marker: PhantomData,
-            tim_pin,
             ratio: duty_ratio,
         };
 
-        infrared.init();
         infrared
     }
 
-    pub fn init(&mut self) {
-        self.apply(self.ratio);
+    pub fn init<P>(&mut self, tim_pin: &mut Mutex<P>)
+    where
+        P: PwmPin<Duty = u16>,
+    {
+        self.apply(self.ratio, tim_pin);
     }
 
-    pub fn apply(&mut self, mut duty_ratio: f32) {
+    pub fn apply<P>(&mut self, mut duty_ratio: f32, tim_pin: &mut Mutex<P>)
+    where
+        P: PwmPin<Duty = u16>,
+    {
         if duty_ratio > 1.0 {
             duty_ratio = 1.0;
         } else if duty_ratio < 0.0 {
             duty_ratio = 0.0;
         }
-        self.tim_pin
+        tim_pin.lock().set_duty(tim_pin.lock().get_max_duty());
+        tim_pin
             .lock()
-            .set_duty(self.tim_pin.lock().get_max_duty());
-        self.tim_pin
-            .lock()
-            .set_duty((duty_ratio * self.tim_pin.lock().get_max_duty() as f32) as u16);
+            .set_duty((duty_ratio * tim_pin.lock().get_max_duty() as f32) as u16);
+        self.ratio = duty_ratio;
     }
 
     #[allow(unused)]
-    fn update_value(&mut self) {
-        self.value = block!(self.adc.lock().read(&mut self.adc_pin)).unwrap() as u16;
+    fn update_value<T>(&mut self, adc: &mut Mutex<T>)
+    where
+        T: OneShot<ADC, u16, AdcPin>,
+        <T as OneShot<ADC, u16, AdcPin>>::Error: core::fmt::Debug,
+    {
+        self.value = block!(adc.lock().read(&mut self.adc_pin)).unwrap() as u16;
     }
 
     #[allow(unused)]
