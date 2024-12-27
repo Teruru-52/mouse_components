b25f274982edc61f6604a8f471a9bd08075e1fd3
ReijiTerunuma <reiji.terunuma@newinov.com>
2024-12-28 00:20:52 +0900

    feat: add magnetic encoder

--- sensors2/src/encoder.rs
+++ sensors2/src/encoder.rs
@@ -1,5 +1,12 @@
-use embedded_hal::Qei;
-use uom::si::{angle::revolution, f32::Angle};
+use crate::wait_ok;
+use core::convert::Infallible;
+use core::marker::PhantomData;
+use embedded_hal::{
+    blocking::delay::DelayMs, blocking::spi::Transfer, digital::v2::OutputPin, timer::CountDown,
+    Qei,
+};
+use nb::block;
+use uom::si::{angle::revolution, f32::Angle, f32::AngularVelocity};
 
 pub struct MA702GQ<Q>
 where
@@ -44,5 +51,116 @@ where
         Ok(Angle::new::<revolution>(
             relative_count as f32 / Self::RESOLUTION_PER_ROTATION,
         ))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct AS5055AError;
+
+impl From<Infallible> for AS5055AError {
+    fn from(_error: Infallible) -> Self {
+        Self
+    }
+}
+
+pub struct AS5055A<T> {
+    cs: T,
+    radius: Angle,
+    angle: Angle,
+    prev_angle: Angle,
+}
+
+impl<T> AS5055A<T>
+where
+    T: OutputPin,
+{
+    //RA: register address
+    const ANGLE_OUT_H: u8 = 0x3F;
+    const ANGLE_OUT_L: u8 = 0xFF;
+
+    pub fn new<S, V, W>(
+        spi: &mut S,
+        cs: T,
+        delay: &mut V,
+        timer: &mut W,
+        tire_radius: Angle,
+    ) -> Self
+    where
+        S: Transfer<u8>,
+        V: DelayMs<u32>,
+        W: CountDown,
+    {
+        let mut as5055a = Self {
+            cs,
+            radius: tire_radius,
+            angle: Default::default(),
+            prev_angle: Default::default(),
+        };
+
+        as5055a.init(spi, delay, timer);
+
+        as5055a
+    }
+
+    pub fn init<S, V, W>(&mut self, spi: &mut S, delay: &mut V, timer: &mut W)
+    where
+        S: Transfer<u8>,
+        V: DelayMs<u32>,
+        W: CountDown,
+    {
+        self.angle = self.angle(spi).unwrap();
+        self.prev_angle = self.angle;
+    }
+
+    fn assert(&mut self) -> Result<(), AS5055AError> {
+        self.cs.set_low().map_err(|_| AS5055AError)
+    }
+
+    fn deassert(&mut self) -> Result<(), AS5055AError> {
+        self.cs.set_high().map_err(|_| AS5055AError)
+    }
+
+    //size of buffer should be equal to {data length}+1
+    fn read_from_registers<'w, S: Transfer<u8>>(
+        &mut self,
+        spi: &mut S,
+        address: u8,
+        buffer: &'w mut [u8],
+    ) -> Result<&'w [u8], AS5055AError> {
+        self.assert()?;
+        let res = Self::_read_from_registers(spi, address, buffer);
+        self.deassert()?;
+        res
+    }
+
+    fn _read_from_registers<'w, S: Transfer<u8>>(
+        spi: &mut S,
+        address: u8,
+        buffer: &'w mut [u8],
+    ) -> Result<&'w [u8], AS5055AError> {
+        buffer[0] = address | 0x80;
+        let buffer = spi.transfer(buffer).map_err(|_| AS5055AError)?;
+        Ok(&buffer[1..])
+    }
+
+    #[inline]
+    fn connect_raw_data(&self, higher: u8, lower: u8) -> i16 {
+        (((higher as u16) << 8 | lower as u16) & 0x3FFC >> 2) as i16
+    }
+
+    fn convert_raw_data_to_angle(&mut self, raw_value: i16) -> Angle {
+        self.radius * raw_value as f32
+    }
+
+    pub fn angle<S: Transfer<u8>>(&mut self, spi: &mut S) -> nb::Result<Angle, AS5055AError> {
+        let mut buffer = [0; 3];
+        let buffer = self.read_from_registers(spi, Self::ANGLE_OUT_H, &mut buffer)?;
+        self.angle = -self.convert_raw_data_to_angle(self.connect_raw_data(buffer[0], buffer[1]));
+        Ok(self.angle)
+    }
+
+    pub fn dist_angle<S: Transfer<u8>>(&mut self, spi: &mut S) -> Angle {
+        self.angle(spi).unwrap();
+        self.angle - self.prev_angle
     }
 }
